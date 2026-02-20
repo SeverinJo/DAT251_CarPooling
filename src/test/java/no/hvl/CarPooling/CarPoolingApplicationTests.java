6c2af7b98ec5b828a59ba994a78cf257c7c4121f
Severin <sevjoh23@gmail.com>
2026-02-20 11:35:38 +0100

    My second commit

---
+++ src/test/java/no/hvl/CarPooling/CarPoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.CarPooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
