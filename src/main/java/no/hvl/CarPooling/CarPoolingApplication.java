07ef5e84134c4435d95e8ed133e779ec8a8fdf8e
Ole Martin Amundsen <111424973+671454@users.noreply.github.com>
2026-02-23 11:15:44 +0100

    Test av push

--- src/main/java/no/hvl/CarPooling/CarPoolingApplication.java
+++ src/main/java/no/hvl/CarPooling/CarPoolingApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarPoolingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CarPoolingApplication.class, args);
+		System.out.print("Ole var her!");
 	}
 
 }
