b6eeb62ff12133bd70e4079936ae6b2ef07188ef
599061 <dag__aa@hotmail.com>
2026-03-24 11:45:19 +0100

    implemented ProfilePage.tsx

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,29 @@
+  import { useEffect, useState } from "react"
+  import { currentUser, getBookings } from "../api/mock"
+  import type { Booking } from "../types"
+
 function ProfilePage() {
-    return <div>ProfilePage</div>;
+        const [bookings, setBookings] = useState<Booking[]>([])
+        const [loading, setLoading] = useState(true)
+
+        useEffect(() => {
+          getBookings().then((data) => {
+            setBookings(data)
+            setLoading(false)
+          })
+        }, [])
+
+        return (
+          <div>
+            <h1>{currentUser.name}</h1>
+            <p>Rating: {currentUser.rating}</p>
+            <p>{currentUser.email} </p>
+            {loading ? <p>Loading...</p> : bookings.map(b => (
+              <div key={b.id}>{/* booking card */}</div>
+            ))}
+          </div>
+        )
+
 }
 
 export default ProfilePage;
