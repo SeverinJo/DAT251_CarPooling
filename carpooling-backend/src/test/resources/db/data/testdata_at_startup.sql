3289e66a15b42d8e5f8cd8284477afcb5aa6449e
doningen <doningen@localhost.localdomain>
2026-03-25 23:10:56 +0100

    4rde forsøk usercrud

--- carpooling-backend/src/test/resources/db/data/testdata_at_startup.sql
+++ carpooling-backend/src/test/resources/db/data/testdata_at_startup.sql
@@ -2,4 +2,5 @@
 INSERT INTO user_table (username, email, password)
 VALUES ('alice', 'alice@example.com', 'password1'),
        ('bob', 'bob@example.com', 'password2'),
-       ('charlie', 'charlie@example.com', 'password3');
+       ('charlie', 'charlie@example.com', 'password3')
+ON CONFLICT DO NOTHING;
