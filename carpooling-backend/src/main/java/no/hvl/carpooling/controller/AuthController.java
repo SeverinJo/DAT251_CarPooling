b9aa5790ecf98742bc511f66c36b2eacd9d35645
thtroy <89257822+thtroy@users.noreply.github.com>
2026-04-06 01:31:56 +0200

    Setup Spring Security , Auth & JWT

---
+++ carpooling-backend/src/main/java/no/hvl/carpooling/controller/AuthController.java
@@ -0,0 +1,53 @@
+package no.hvl.carpooling.controller;
+
+import jakarta.transaction.Transactional;
+import no.hvl.carpooling.repository.UserRepository;
+import no.hvl.carpooling.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class AuthController {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(JwtService jwtService,
+                          UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    @Transactional
+    public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
+        var userOpt = userRepository.findByUsername(username);
+
+        if (userOpt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid username or password");
+        }
+        System.out.println(userOpt.get().getUsername());
+
+
+        var user = userOpt.get();
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid username or password");
+        }
+
+        String token = jwtService.generateToken(username);
+        return ResponseEntity.ok(token);
+    }
+}
