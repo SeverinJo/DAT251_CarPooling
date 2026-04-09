9fd35918366876b61fce65078eb62796741a7e40
Severin <sevjoh23@gmail.com>
2026-04-09 12:50:56 +0200

    docker postgresql, vite.config.ts, generates api doc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-server: {
-  proxy: {
-    '/api': 'http://localhost:8080',
-    '/login': 'http://localhost:8080',
-    '/register': 'http://localhost:8080',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/login': 'http://localhost:8080',
+      '/register': 'http://localhost:8080',
+    }
   }
-}
+})
